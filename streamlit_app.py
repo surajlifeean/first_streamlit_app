982f348526ea4da1f68c6cc1edcd131778c61e43
Suraj Jeswara <surajjeswara@yahoo.com>
2023-07-13 00:49:24 +0530

    Update streamlit_app.py
    added header and title

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,8 @@
+import streamlit
+
+streamlit.title("My Parents New Healthy Diner")
+streamlit.header("Breakfast Menu")
+streamlit.text("Item 1")
+streamlit.text("Item 2")
+streamlit.text("Item 3")
 
