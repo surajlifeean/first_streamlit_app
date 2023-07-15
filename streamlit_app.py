977514d3e340d1442a5e9a2a1a28e9db11efa6e8
Suraj Jeswara <surajjeswara@yahoo.com>
2023-07-15 19:35:30 +0530

    Update streamlit_app.py
    added field to take input from user

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,6 +20,9 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon"
 
 streamlit.text(fruityvice_response.json())
 
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 
 streamlit.dataframe(fruityvice_normalized)
